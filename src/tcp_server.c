bc8079934df754742477ce5189bfae463142b4b0
Quinn Handley <handleyq@oregonstate.edu>
2019-11-21 09:09:53 -0800

    Modified tcp server for debugging via uart

--- src/tcp_server.c
+++ src/tcp_server.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "socket.h"
 #include "tcp_server.h"
+#include "uart.h"
+
+#define MAC         0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00
+#define IP          192, 168, 1, 10 
+#define SUBNET      255, 255, 255, 0 
+#define GATEWAY     192, 168, 1, 1 
+#define DNS         8, 8, 8, 8
 
 #if LOOPBACK_MODE == LOOPBACK_MAIN_NOBLOCK
+void initSPI()
+{
+    DDRB |= (1 << PB5) | (1 << PB3) | (1 << PB2); //sck, mosi, ss outputs
+    SPCR |= (1 << SPE) | (1 << MSTR); //
+}
 
 int32_t loopback_tcps(uint8_t sn, uint8_t* buf, uint16_t port)
 {
    int32_t ret;
    uint16_t size = 0, sentsize=0;
 
-#ifdef _LOOPBACK_DEBUG_
-   uint8_t destip[4];
-   uint16_t destport;
-#endif
-
    switch(getSn_SR(sn))
    {
       case SOCK_ESTABLISHED :
          if(getSn_IR(sn) & Sn_IR_CON)
          {
-#ifdef _LOOPBACK_DEBUG_
-			getSn_DIPR(sn, destip);
-			destport = getSn_DPORT(sn);
-
-			printf("%d:Connected - %d.%d.%d.%d : %d\r\n",sn, destip[0], destip[1], destip[2], destip[3], destport);
-#endif
 			setSn_IR(sn,Sn_IR_CON);
          }
 		 if((size = getSn_RX_RSR(sn)) > 0) // Don't need to check SOCKERR_BUSY because it doesn't not occur.
@@ -51,29 +53,13 @@ int32_t loopback_tcps(uint8_t sn, uint8_t* buf, uint16_t port)
          }
          break;
       case SOCK_CLOSE_WAIT :
-#ifdef _LOOPBACK_DEBUG_
-         //printf("%d:CloseWait\r\n",sn);
-#endif
          if((ret = disconnect(sn)) != SOCK_OK) return ret;
-#ifdef _LOOPBACK_DEBUG_
-         printf("%d:Socket Closed\r\n", sn);
-#endif
          break;
       case SOCK_INIT :
-#ifdef _LOOPBACK_DEBUG_
-    	 printf("%d:Listen, TCP server loopback, port [%d]\r\n", sn, port);
-#endif
          if( (ret = listen(sn)) != SOCK_OK) return ret;
          break;
       case SOCK_CLOSED:
-#ifdef _LOOPBACK_DEBUG_
-         //printf("%d:TCP server loopback start\r\n",sn);
-#endif
          if((ret = socket(sn, Sn_MR_TCP, port, 0x00)) != sn) return ret;
-#ifdef _LOOPBACK_DEBUG_
-         //printf("%d:Socket opened\r\n",sn);
-#endif
-         break;
       default:
          break;
    }
@@ -84,12 +70,125 @@ int32_t loopback_tcps(uint8_t sn, uint8_t* buf, uint16_t port)
 
 int main()
 {
-    DDRB |= 0xFF;
+    initUART();
+    initSPI();
+
+    //uint8_t rw_buffer[2000];
+    char buffer1[10];
+    char buffer2[10];
+
+    uint8_t ip[] = {192, 168, 1, 10};
+    char okay[] = "OKAY";
+    char err[] = "ERROR";
+
+    struct wiz_NetInfo_t network_config = 
+    {
+        {MAC},
+        {IP},
+        {SUBNET},
+        {GATEWAY},
+        {DNS},
+        1
+    };
+
+    struct wiz_NetInfo_t temp;
+
+    uint8_t txsize[8] = {16, 0, 0, 0, 0, 0, 0, 0};
+    uint8_t rxsize[8] = {16, 0, 0, 0, 0, 0, 0, 0};
+
+    _delay_ms(5000);
+
+    int8_t init_ret = wizchip_init(txsize, rxsize); //software reset and configure tx/rx buffers
+
+    _delay_ms(2000);
+
+    wizchip_setnetinfo(&network_config);
+    wizchip_getnetinfo(&temp);
+
+    //setRTR(0xFFFF); //maximum value
+
+    //int8_t ret = socket(0, Sn_MR_TCP, 8080, 0); //establish socket 0 in TCP mode port 8080 no flags
+    setSn_MR(0, 0x01); //TCP
+    setSn_PORT(0, 8080); //port
+    setSn_CR(0, Sn_CR_OPEN);
+    _delay_ms(100);
+
+    //while(getSn_CR(0));
 
     while(1)
     {
-        PORTB ^= 0xFF;
-        _delay_ms(500);
+        itoa(init_ret, buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        /*
+        itoa(ret, buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+        */
+
+        itoa(getSn_SR(0), buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        itoa(getSn_MR(0), buffer1, 16);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        itoa(getSn_PORT(0), buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        uint8_t rcr = 8; // getRCR();
+
+        itoa(rcr, buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        itoa(getSn_IR(0), buffer1, 16);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        itoa(getIR(), buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        /*
+        itoa(temp.mac[0], buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        itoa(temp.ip[0], buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        itoa(temp.sn[0], buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        itoa(temp.gw[0], buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+
+        itoa(temp.dns[0], buffer1, 10);
+        writeString(buffer1);
+        putByte('\r');
+        putByte('\n');
+        */
+
+        _delay_ms(1000);
     }
 
     return 0;
