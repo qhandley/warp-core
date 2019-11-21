5c10d76d545875493a00e87517d4fe11aac2103d
Quinn Handley <handleyq@oregonstate.edu>
2019-11-21 11:16:49 -0800

    Removed driver submodule

---
+++ libs/ioLibrary_Driver/Ethernet/wizchip_select.h
@@ -0,0 +1,10 @@
+#ifndef _WIZCHIP_SELECT_H_
+#define _WIZCHIP_SELECT_H_
+
+//Select W5500
+#define _WIZCHIP_               W5500
+//Set I/O mode to use SPI rather than BUS
+#define _WIZCHIP_IO_MODE_       _WIZCHIP_IO_MODE_SPI_VDM_
+
+#endif //_WIZCHIP_SELECT_H_
+
