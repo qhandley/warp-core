df4f2222a23e2f0784972c6b601696e61dfb1cf0
Quinn Handley <handleyq@oregonstate.edu>
2019-11-25 10:45:07 -0800

    Timer interrupt driven receive

--- src/tcp_server.c
+++ src/tcp_server.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "socket.h"
 #include "tcp_server.h"
@@ -17,7 +18,35 @@
 void initSPI()
 {
     DDRB |= (1 << PB5) | (1 << PB3) | (1 << PB2); //sck, mosi, ss outputs
-    SPCR |= (1 << SPE) | (1 << MSTR); //
+    SPCR |= (1 << SPE) | (1 << MSTR);
+}
+
+void initTCNT0()
+{
+    TIMSK0 |= (1 << TOIE0); //enable overflow interrupt 
+    TCCR0B |= (1 << CS02) | (1 << CS00); //1024 prescaling
+}
+
+void disableTCNT0()
+{
+    TCCR0B = 0;
+}
+
+void enableTCNT0()
+{
+    TCCR0B |= (1 << CS02) | (1 << CS00); //1024 prescaling
+}
+
+ISR(TIMER0_OVF_vect)
+{
+    uint8_t s0_ir = getSn_IR(0);
+
+    if(!bit_is_clear(s0_ir, 2))
+    {
+        writeString("recv interrupt received!\r\n");
+        setSn_IR(0, (s0_ir & (1 << 2))); //clear interrupt
+        PORTB ^= (1 << 0);
+    }
 }
 
 int32_t loopback_tcps(uint8_t sn, uint8_t* buf, uint16_t port)
@@ -43,6 +72,7 @@ int32_t loopback_tcps(uint8_t sn, uint8_t* buf, uint16_t port)
 
 			while(size != sentsize)
 			{
+                writeNumChar("First: ", *buf, 10);
 				ret = send(sn, buf+sentsize, size-sentsize);
 				if(ret < 0)
 				{
@@ -71,6 +101,8 @@ int main()
 {
     initUART();
     initSPI();
+    initTCNT0();
+    DDRB |= (1 << 0);
 
     uint8_t buf[1000];
 
@@ -90,7 +122,7 @@ int main()
     uint8_t rxsize[8] = {1, 0, 0, 0, 0, 0, 0, 0};
 
     //setup delay
-    _delay_ms(5000);
+    _delay_ms(2000);
 
     writeNumChar("Init return: ", wizchip_init(txsize, rxsize), 10);
     wizchip_setnetinfo(&network_config);
@@ -104,10 +136,14 @@ int main()
     writeNumChar("version: ", getVERSIONR(), 16);
     writeNumShort("retry count: ", getRTR(), 10);
 
+    sei(); //enable global interrupts
+
     while(1)
     {
+        disableTCNT0();
         loopback_tcps(0, buf, 8080);
         writeNumChar("Socket 0 SR: ", getSn_SR(0), 16);
+        enableTCNT0();
         _delay_ms(250);
     }
 
